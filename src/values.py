a3b9bebaaa8530f6b9090646c3cfac5aa92707a6
Vincent-Nittolo <160050293+Vincent-Nittolo@users.noreply.github.com>
2024-12-06 14:50:21 -0500

    Update values.py

--- src/values.py
+++ src/values.py
@@ -10,6 +10,7 @@ class Values:
         self._GAME = False
         self._START = True
         self._SELEC = False
+        self._CHOOSE = True
         self._SETTINGS = False
         self._INFO = False
         self._WIN = False
@@ -42,6 +43,13 @@ class Values:
         self._black = (0, 0, 0)
         self._blue = (27, 144, 221)
         self._current = self.blue
+        self._solos = False
+        self._duos = False
+        self._trios = False
+        self._squads = False
+        self._jx = 0
+        self._jy = 0
+        self._num_players = 0
 
     @property
     def DICE(self):
@@ -74,6 +82,14 @@ class Values:
     @START.setter
     def START(self, value):
         self._START = value
+
+    @property
+    def CHOOSE(self):
+        return self._CHOOSE
+
+    @CHOOSE.setter
+    def CHOOSE(self, value):
+        self._CHOOSE = value
 
     @property
     def SELEC(self):
@@ -297,3 +313,59 @@ class Values:
     @current.setter
     def current(self, value):
         self._current = value
+
+    @property
+    def solos(self):
+        return self._solos
+
+    @solos.setter
+    def solos(self, value):
+        self._solos = value
+
+    @property
+    def duos(self):
+        return self._duos
+
+    @duos.setter
+    def duos(self, value):
+        self._duos = value
+
+    @property
+    def trios(self):
+        return self._trios
+
+    @trios.setter
+    def trios(self, value):
+        self._trios = value
+
+    @property
+    def squads(self):
+        return self._squads
+
+    @squads.setter
+    def squads(self, value):
+        self._squads = value
+
+    @property
+    def jx(self):
+        return self._jx
+
+    @jx.setter
+    def jx(self, value):
+        self._jx = value
+
+    @property
+    def jy(self):
+        return self._jy
+
+    @jy.setter
+    def jy(self, value):
+        self._jy = value
+
+    @property
+    def num_players(self):
+        return self._num_players
+
+    @num_players.setter
+    def num_players(self, value):
+        self._num_players = value
