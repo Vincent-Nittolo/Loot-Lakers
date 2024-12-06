dd1fb84181b562f49f3ac2d9351537daffad4153
Vincent-Nittolo <160050293+Vincent-Nittolo@users.noreply.github.com>
2024-12-06 14:49:59 -0500

    Update render.py

--- src/render.py
+++ src/render.py
@@ -14,3 +14,11 @@ def render_text_with_bg(font, scrn, text, color, bg_color, location):
     textRect = text.get_rect()
     textRect.center = location
     scrn.blit(text, textRect)
+
+def render_circle(player, scrn, pygame):
+    if player.space == 8 and player.jail:
+        player.loc_x = 170 + 10*player.num
+        player.loc_y = 585 + 10*player.num
+        pygame.draw.circle(scrn, player.color, [player.loc_x, player.loc_y], 10, 0)
+    else:
+        pygame.draw.circle(scrn, player.color, [player.loc_x, player.loc_y], 10, 0)
